2efda86f30d49ddbbc65a3c7e95f2df9f96edd36
Thanatchaporn Intharasoon <57722046+thanat-koi@users.noreply.github.com>
2020-09-13 23:06:32 +0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "./component/Navbar";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Input.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+export default funcrion Input() {
+  return (
+    <div className="Input"></div>
+  )
+}
